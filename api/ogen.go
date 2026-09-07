ac96c9a1839502bc9d5ec818b3551e7d7aec4c43
Dmitry Osin <d@osin.pro>
2026-09-07 20:23:19 +0300

    [+] open api

---
+++ api/ogen.go
@@ -0,0 +1,5 @@
+package api
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config kafka/oapi-codegen-kafka.yaml kafka/cards-operations-kafka.yaml
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config rest/oapi-codegen-rest.yaml rest/cards-operations-rest.yaml
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "{ \"status\" : \"ok\" }")
+}
+
+func main() {
+	http.HandleFunc("/health", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
